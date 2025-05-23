eb58c4c414d2b004f4bb9e6895be8b3050bd459d
EzequielZarza <ezequielm.zarza@gmail.com>
2025-05-22 22:01:08 -0300

    vs c++ set up for unreal

---
+++ Intermediate/Build/Win64/TP_PreEntregaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.16.27054
+#include "D:/UTN-e-learning/Unreal-Engine/Unreal-Projects/TP_PreEntrega/Intermediate/Build/Win64/TP_PreEntregaEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/TP_PreEntrega/BaseBullet.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TP_PREENTREGA_BaseBullet_generated_h
+#error "BaseBullet.generated.h already included, missing '#pragma once' in BaseBullet.h"
+#endif
+#define TP_PREENTREGA_BaseBullet_generated_h
+
+#define TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_SPARSE_DATA
+#define TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_RPC_WRAPPERS
+#define TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesABaseBullet(); \
+	friend struct Z_Construct_UClass_ABaseBullet_Statics; \
+public: \
+	DECLARE_CLASS(ABaseBullet, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/TP_PreEntrega"), NO_API) \
+	DECLARE_SERIALIZER(ABaseBullet)
+
+
+#define TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesABaseBullet(); \
+	friend struct Z_Construct_UClass_ABaseBullet_Statics; \
+public: \
+	DECLARE_CLASS(ABaseBullet, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/TP_PreEntrega"), NO_API) \
+	DECLARE_SERIALIZER(ABaseBullet)
+
+
+#define TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ABaseBullet(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ABaseBullet) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABaseBullet); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABaseBullet); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABaseBullet(ABaseBullet&&); \
+	NO_API ABaseBullet(const ABaseBullet&); \
+public:
+
+
+#define TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ABaseBullet(ABaseBullet&&); \
+	NO_API ABaseBullet(const ABaseBullet&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ABaseBullet); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ABaseBullet); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ABaseBullet)
+
+
+#define TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_PRIVATE_PROPERTY_OFFSET
+#define TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_9_PROLOG
+#define TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_PRIVATE_PROPERTY_OFFSET \
+	TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_SPARSE_DATA \
+	TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_RPC_WRAPPERS \
+	TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_INCLASS \
+	TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_PRIVATE_PROPERTY_OFFSET \
+	TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_SPARSE_DATA \
+	TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_INCLASS_NO_PURE_DECLS \
+	TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TP_PREENTREGA_API UClass* StaticClass<class ABaseBullet>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TP_PreEntrega_Source_TP_PreEntrega_Actors_BaseBullet_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
